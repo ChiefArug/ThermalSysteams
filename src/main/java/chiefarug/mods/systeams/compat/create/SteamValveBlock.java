d2d8212a210815a5ed08715ab12bbdc7d1b9cb7b
ChiefArug <73862885+ChiefArug@users.noreply.github.com>
2023-04-21 14:24:29 +1200

    its a start, i guess. No more progress till something happens to unhard code the boiler

---
+++ src/main/java/chiefarug/mods/systeams/compat/create/SteamValveBlock.java
@@ -0,0 +1,9 @@
+package chiefarug.mods.systeams.compat.create;
+
+import net.minecraft.world.level.block.FaceAttachedHorizontalDirectionalBlock;
+
+public class SteamValveBlock extends FaceAttachedHorizontalDirectionalBlock {
+	public SteamValveBlock(Properties properties) {
+		super(properties);
+	}
+}
