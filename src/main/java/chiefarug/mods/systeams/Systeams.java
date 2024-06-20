a693755d9d073c6273a0d7e59682bcfd0314c66b
ChiefArug <73862885+ChiefArug@users.noreply.github.com>
2024-06-20 21:41:46 +1200

    Cleanup and move from our own tooltip event to CoFH's

--- src/main/java/chiefarug/mods/systeams/Systeams.java
+++ src/main/java/chiefarug/mods/systeams/Systeams.java
@@ -3,21 +3,14 @@ package chiefarug.mods.systeams;
 import chiefarug.mods.systeams.compat.mekanism.SysteamsMekanismCompat;
 import chiefarug.mods.systeams.compat.pneumaticcraft.SysteamsPNCRCompat;
 import chiefarug.mods.systeams.networking.RecipeCheckerChannel;
-import cofh.core.config.CoreClientConfig;
-import cofh.lib.util.Utils;
-import cofh.lib.util.constants.ModIds;
-import cofh.lib.util.helpers.StringHelper;
+import cofh.core.event.CoreClientEvents;
 import com.mojang.logging.LogUtils;
 import me.desht.pneumaticcraft.api.tileentity.IAirHandlerMachine;
 import mekanism.api.chemical.gas.IGasHandler;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.common.capabilities.CapabilityToken;
-import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModList;
@@ -30,8 +23,6 @@ import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 
-import java.util.List;
-
 import static net.minecraftforge.eventbus.api.EventPriority.LOWEST;
 
 @Mod("systeams")
@@ -42,7 +33,7 @@ public class Systeams {
     public static final String MODID = "systeams";
 
     public static final Capability<IAirHandlerMachine> AIR_HANDLER_CAPABILITY = CapabilityManager.get(new CapabilityToken<>() {});
-    public static final Capability<IGasHandler> GAS_HANDLER_CAPABILITY = CapabilityManager.get(new CapabilityToken<IGasHandler>() {});
+    public static final Capability<IGasHandler> GAS_HANDLER_CAPABILITY = CapabilityManager.get(new CapabilityToken<>() {});
 
     public Systeams() {
         var bus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -57,6 +48,8 @@ public class Systeams {
         if (ModList.get().isLoaded("mekanism")) {
             SysteamsMekanismCompat.activateMechanisedManifold(bus);
         }
+
+        CoreClientEvents.addNamespace(MODID);
     }
 
     /**
@@ -84,30 +77,4 @@ public class Systeams {
             first = false;
         }
     }
-
-    @SubscribeEvent
-    static void tooltipEvent(ItemTooltipEvent event) {
-        // copy this because the default check only does cofh namespaces
-        ItemStack stack = event.getItemStack();
-        List<Component> tooltip = event.getToolTip();
-        String modid = Utils.getModId(stack.getItem());
-        if (!(modid.equals(MODID) || /*CoreClientEvents.NAMESPACES*/ modid.equals(ModIds.ID_THERMAL))) return;
-
-
-        if (CoreClientConfig.enableKeywords.get()) {
-            String translationKey = stack.getDescriptionId() + ".keyword";
-            if (StringHelper.canLocalize(translationKey)) {
-                if (tooltip.get(0) instanceof MutableComponent mutable) {
-                    mutable.append(StringHelper.getKeywordTextComponent(translationKey));
-                }
-            }
-        }
-
-//        if (CoreClientConfig.enableItemDescriptions.get()) {
-//            if (stack.getItem().equals(SysteamsRegistry.Items.RF_COIL.get())) {
-//                tooltip.add(Component.translatable(stack.getDescriptionId() + ".desc").withStyle(ChatFormatting.GOLD));
-//            }
-//        }
-    }
-
 }
